3aa1ad9159e933df8df60f5a391addd3ced592e9
rijoabraham <hdalive@hotmail.com>
2023-08-23 09:35:55 +0530

    fix

--- javascript/rev-slider.js
+++ javascript/rev-slider.js
@@ -267,9 +267,10 @@ var RevSlider = function() {
                         outof: "pause",
                         visible_area: "80%"
                     },
-                    responsiveLevels:[2220,1183,975,750],
+                    responsiveLevels:[2220,1443,975,750],
                     gridwidth:[1170,970,760,320],
-                    gridheight: [910,700,620,625],
+                    gridheight: [950,750,620,625],
+                    // gridheight: [1110,800,620,625],
                     lazyType: "smart",
                     parallax: {
                         type: "scroll",
